9c663109e25c9683001bde3c9736048f96d34e16
Oszterr <rodrigooszter@outlook.com>
2024-10-14 22:50:03 -0300

    Added initial user service methods.

--- MyGameListBackend/Services/UserService.cs
+++ MyGameListBackend/Services/UserService.cs
@@ -1,24 +1,28 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using MyGameList.Data;
 using MyGameList.Models;
 
 namespace MyGameList.Services
 {
-    public class UserService
-    {
-        public UserService()
-        {
 
-        }
+    public class UserService(MyGameListContext context)
+    {
+        public readonly MyGameListContext _context = context;
 
         public IEnumerable<User> GetAll()
         {
-            throw new NotImplementedException();
+            return [.. _context.Users.AsNoTracking()];
         }
 
-        public User? GetById(int id)
+        public User GetById(int id)
         {
-            throw new NotImplementedException();
+            return _context.Users
+            .Include(p => p.Tracker)
+            .AsNoTracking()
+            .SingleOrDefault(p => p.Id == id);
         }
     }
 }
