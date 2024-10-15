0175824bc9561ba3c811853a0ece60608dad3d61
Diego <diegodutra2000@hotmail.com>
2024-10-14 22:44:22 -0300

    OS

---
+++ MyGameListBackend/Models/Platform.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace MyGameList.Models
+{
+    public class Platform (string Name, string OperatingSystem, OperationalSystem OperationalSystem)
+    {
+
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(80)]
+        public string Name { get; set; } = Name;
+
+        [Required]
+        [MaxLength(80)]
+        public string OperatingSystem { get; set; } = OperatingSystem;
+
+        [Required]
+        public OperationalSystem OperationalSystem { get; set; } = OperationalSystem;
+
+        [Required]
+        public ICollection<Game> Game { get; set; }
+
+    }
+}
